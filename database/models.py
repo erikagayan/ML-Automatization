fd0878c21e33d5211f0429e4f0151ff93dab006f
erikagayan <erik.agayan1@gmail.com>
2025-09-10 18:07:05 +0300

    Add models.py

--- database/models.py
+++ database/models.py
@@ -3,7 +3,7 @@ from datetime import datetime, UTC
 from database.engine import Base
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Column, String, Boolean, DateTime
+from sqlalchemy import String, Boolean, DateTime
 
 class Task(Base):
     __tablename__ = "tasks"
