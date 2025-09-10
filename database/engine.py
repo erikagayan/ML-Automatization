2afad742fc047ec2b4c57eb935f06adc621f0222
erikagayan <erik.agayan1@gmail.com>
2025-09-10 03:33:22 +0300

    Delete pool_size and max_overflow in engine.py

--- database/engine.py
+++ database/engine.py
@@ -1,11 +1,11 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 
 SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://admin:admin@localhost:5432/ml_automatization"
 
 # Creates an engine that manages connections to the database.
-engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False, pool_size=10, max_overflow=20)
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=False)
 
 # Factory for creating sessions
 SessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
