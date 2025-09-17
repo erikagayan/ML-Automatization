5117053aa7c86b06b5a3c2b951e7b843127e3bdb
erikagayan <erik.agayan1@gmail.com>
2025-09-17 13:48:01 +0300

    Add ruff and used to celery_app.py

--- celery_app.py
+++ celery_app.py
@@ -1,6 +1,6 @@
 import sys
-from celery import Celery
 from pathlib import Path
+from celery import Celery
 
 # Add the project root directory to sys.path
 project_root = Path(__file__).resolve().parent
@@ -11,11 +11,11 @@ app = Celery("tasks", broker="redis://localhost:6379/0")
 # app = Celery("tasks", broker="redis://redis:6379/0")
 
 # Searches for tasks in the tasks.py file
-app.autodiscover_tasks(['tasks'])
+app.autodiscover_tasks(["tasks"])
 
 app.conf.beat_schedule = {
-    'fetch-users-every-5-minutes': {
-        'task': 'tasks.fetch_and_save_users_task',
-        'schedule': 10,  # 10 seconds
+    "fetch-users-every-5-minutes": {
+        "task": "tasks.fetch_and_save_users_task",
+        "schedule": 10,  # 10 seconds
     },
 }
